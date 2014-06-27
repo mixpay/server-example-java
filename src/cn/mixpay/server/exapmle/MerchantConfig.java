4d1d524eee91c3cbe109231dc3fcb7dc9875cca6
Xu yannan <xyn0563@gmail.com>
2014-06-27 18:06:19 +0800

    增加订单查询示例

---
+++ src/cn/mixpay/server/exapmle/MerchantConfig.java
@@ -0,0 +1,17 @@
+package cn.mixpay.server.exapmle;
+
+/**
+ * Created by xyn0563 on 6/27/14.
+ */
+public class MerchantConfig {
+    /**
+     * mixpay给app分配的唯一标识
+     */
+    public static final String APP_KEY = "6018561003558745";
+
+    /**
+     * 商户的私钥
+     */
+    public static final String PRIVATE_KEY = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAKzgKcAVdV938q/fK3NJDpThSvaXJQPooipTqVDFPHK+uhDUWIu0frwBy96lFWPQCLcXvOKhGi/WDmSaYVY8jIp53qm8r5cReAp1TqctV4lrHBxfWc6INeHj1Qk0jBwGhENQk/ySvEnk3/zTKOzWJh8blXLXIYrwD9mVqMBpDyDDAgMBAAECgYAhoTXd/Q6pNL9MQUDFm4evpKgdkkeMHBw32bCNWuEofTva/EQBYWpqDntnY3vmv9iCLab7+1UJyz1firy2tu6ry4+dlu/LdbEHqMwBPfcKHfwT2EG6Q41mA7KMlf6def+fBHDSRvWGFW82oiMVAUSvchr7lELGbarZuyHL1pFOiQJBAN7KHfNhmPoFhvFT6si5PXpl68a2k7HqjLIU23SiIQVK6RgqvNJolqzTKoE8r8AdGx9QDzDZAxuvTsb1JnmGEA0CQQDGpUeah9XIAEBfGWW1UHHe9QDmRoeynm678zdlsMNZy4O0U/1ucdbcnq5uFKglSDuxCvFURPuP1qUSipQVyPAPAkEAgFGjrJGzHZZfYdI2sYPYAA6CHYL9UIVKoitXNzdGk5jQ5sV+2iW5WzOJEMTWaW2aOI/RIA5uNtzjH78FeQsZZQJBAJ7/G9jscIis6tkzkt0Vjo9Ou3GVcnfdp/R4MBcM7M+qvbhQocENDVV5DVS+4/czYdPLVm6E6HWw8F9u3CiztLECQEh+qsa6qkGys2sJeNX0yOzgiqw3Iha1HMpLFIgx+Bo7hmZBtEZGtL1MpdgoYAv7bwHm3oi+FMYR8NmdNC9HrSs=";
+
+}
--- src/cn/mixpay/server/exapmle/SignOrderExample.java
+++ src/cn/mixpay/server/exapmle/SignOrderExample.java
@@ -1,33 +1,16 @@
 package cn.mixpay.server.exapmle;
-import cn.mixpay.server.ServerUtil;
-import cn.mixpay.server.SignatureUtils;
-
+import cn.mixpay.server.SignatureTool;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.security.Security;
-
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 /**
  * Created by xyn0563 on 6/5/14.
  * 订单信息签名示例
  */
 public class SignOrderExample {
-    static{
-        try{
-            Security.addProvider(new BouncyCastleProvider());
-            System.out.println("加载RSA的BC加密算法");
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-    public static void main(String[] args) {
-        // mixpay给app分配的key
-        final String appKey = "6018092067351529";
-        // 您的私钥
-        final String privateKey = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAKzgKcAVdV938q/fK3NJDpThSvaXJQPooipTqVDFPHK+uhDUWIu0frwBy96lFWPQCLcXvOKhGi/WDmSaYVY8jIp53qm8r5cReAp1TqctV4lrHBxfWc6INeHj1Qk0jBwGhENQk/ySvEnk3/zTKOzWJh8blXLXIYrwD9mVqMBpDyDDAgMBAAECgYAhoTXd/Q6pNL9MQUDFm4evpKgdkkeMHBw32bCNWuEofTva/EQBYWpqDntnY3vmv9iCLab7+1UJyz1firy2tu6ry4+dlu/LdbEHqMwBPfcKHfwT2EG6Q41mA7KMlf6def+fBHDSRvWGFW82oiMVAUSvchr7lELGbarZuyHL1pFOiQJBAN7KHfNhmPoFhvFT6si5PXpl68a2k7HqjLIU23SiIQVK6RgqvNJolqzTKoE8r8AdGx9QDzDZAxuvTsb1JnmGEA0CQQDGpUeah9XIAEBfGWW1UHHe9QDmRoeynm678zdlsMNZy4O0U/1ucdbcnq5uFKglSDuxCvFURPuP1qUSipQVyPAPAkEAgFGjrJGzHZZfYdI2sYPYAA6CHYL9UIVKoitXNzdGk5jQ5sV+2iW5WzOJEMTWaW2aOI/RIA5uNtzjH78FeQsZZQJBAJ7/G9jscIis6tkzkt0Vjo9Ou3GVcnfdp/R4MBcM7M+qvbhQocENDVV5DVS+4/czYdPLVm6E6HWw8F9u3CiztLECQEh+qsa6qkGys2sJeNX0yOzgiqw3Iha1HMpLFIgx+Bo7hmZBtEZGtL1MpdgoYAv7bwHm3oi+FMYR8NmdNC9HrSs=";
 
-        // 订单信息
+    public static void main(String[] args) {
+
         // 金额，不能为空
         double amount = 2.00;
 
@@ -50,18 +33,16 @@ public class SignOrderExample {
         String productId = "Product001";
 
         // 商品名称，可以为空
-        String productName = "行扫帚";
+        String productName = "飞行扫帚";
 
         // 其他信息，您可以在这里放一些您所需要的信息
         String extData = "";
 
-        // 1.生成待签名串
-        String orderContent = ServerUtil.generateOrderInfoString(amount, appKey, extData, appOrderId, notifyUrl, orderDesc, orderTitle, productDesc,
-                productId, productName);
+        String appUserId = "U0001";
 
-        System.out.println("签名串：" + orderContent);
-        // 2.对订单信息进行签名，并进行utf-8编码
-        String orderSign = SignatureUtils.sign(orderContent, privateKey);
+        // 1.签名
+        String orderSign = SignatureTool.signOrderInfo(amount, MerchantConfig.APP_KEY, extData, appOrderId, notifyUrl, orderDesc, orderTitle, productDesc,
+                productId, productName, appUserId, MerchantConfig.PRIVATE_KEY);
         String encodedOrderSign = null;
         try {
             encodedOrderSign = URLEncoder.encode(orderSign, "utf-8");
@@ -71,6 +52,6 @@ public class SignOrderExample {
 
         System.out.println("encode过后的签名值：" + encodedOrderSign);
 
-        // 3.将订单信息以及encode后的签名值传送给APP，在APP中调用Mixpay支付插件进行支付
+        // 2.将订单信息以及encode后的签名值传送给APP，在APP中调用Mixpay支付插件进行支付
     }
 }
