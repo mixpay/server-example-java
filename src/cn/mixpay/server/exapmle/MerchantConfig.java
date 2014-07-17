9b155551e6b0d2f1d93174a9f336b3f6f9e94b60
xuyannan <xyn0563@gmail.com>
2014-07-17 18:21:56 +0800

    使用新的key

--- src/cn/mixpay/server/exapmle/MerchantConfig.java
+++ src/cn/mixpay/server/exapmle/MerchantConfig.java
@@ -12,13 +12,22 @@ public class MerchantConfig {
     /**
      * 商户的RSA私钥
      */
-    public static final String PRIVATE_KEY = "MIIBVgIBADANBgkqhkiG9w0BAQEFAASCAUAwggE8AgEAAkEAo1XJppX5lmXM37sn\n" +
-            "2x5ylTOVbqrLbyR+WaZqu/Ugq80neDXdGFiGzh6zyk/oq/VG7NZuP1tUmjPPBhY0\n" +
-            "ZceS5QIDAQABAkEAjXSgnYtJiv1DP4hg7h4iciEoi52TbgAXBSFhUsLqtp4mE8/h\n" +
-            "rNFgOAmhmc7cXDwTmIhqGf4axFMPbdzmfU4gyQIhAM+PdP0X7u/9tArWwD5sWBEJ\n" +
-            "0QJob9cMDKSEv3TRZ+4XAiEAyXQfz85jO9xJ46JKh3pBjSadF+ZKUnz/DUYZG6Kt\n" +
-            "gGMCIQCcI8oZHykC5vLK+JQ0IWL6pkzSJ+iCeLEGDkrMwT8SzwIhAKiuz6RVkMoi\n" +
-            "3sv4rKdcO19RoeDNOBZyC661r3hwcw85AiBg6jiMH6A5y2ytU1JjpROcNPCd6MkB\n" +
-            "Fry9mn7CefpBSw==";
+
+    public static final String PRIVATE_KEY =
+            "MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBANkwXDLB0zy+KxA1\n" +
+            "2Ayj0dmdEnlzw7sg93x9yEDiIC190MH2qPYwXLqKb8UxTKT2RbGPzR7fdgnHatqm\n" +
+            "T9M8VAcSFyNErHSj05yWqQmHtfSz070KpvjrCV0y3j+R0vWOZKjCeLcWV2EJQSyN\n" +
+            "HaJQ85zURVZVmQ3EQTrC/b3XUfTBAgMBAAECgYEAgagRImv7gwO0CZVbSRBzMopG\n" +
+            "gSLO8mOX20XrynAz7XRyr5hk/bOZZ/NFDl+D9ebfbEUAmzGj8eqOq8ElXmFhd02B\n" +
+            "fvBTbNk62+XFTOcOmUwnAHihlZvIWNyry6XF0oCUOhSTr88Qj6dVBO6bOdMaMy61\n" +
+            "4uZrkhsBJdT1gRr95VECQQDx1LXt4mF9TbojhIBsBcJVRtanPQVqs0QMq+4qjb9+\n" +
+            "VQQiKqWWJBRcW4d5uB+8YrGUoRQ+imh2pdbKTt+YUl9PAkEA5eoJjg06P7rMpLT3\n" +
+            "ytD9TFuqAlmoI8Zt6uJAiaBKxrS55lU2C/Z7PPnf/nutC1BO/Ro10BXkaoHQV0BM\n" +
+            "kBbm7wJBANiDjW8yY/5//MuX8jLE5Tzcc4rAA32IisCdnlrTFkPRciX08NP/8YrP\n" +
+            "F/hOdXuBEWT04t8rpGO2DxyKiX4jIdkCQQCxY9qNbG1F4JWmey44oPjeYlcQyyYs\n" +
+            "vBvwvzJ2PWOUXrqUGajCoCFD0bNegQPpl8PqCr/8VffPqWsa9rRb9B+nAkAemh3v\n" +
+            "sUTxfxAucCq6F4VbT2UNDFi8keYqlM+5+rqKzbkXqKr/DJwsN0hqZhWdZvjTzjFl\n" +
+            "oLb+Blhz/ugm1D/u";
+
     public static final String SECRET_KEY = "019fafebfebf4a9d28c989254660f46c";
 }
